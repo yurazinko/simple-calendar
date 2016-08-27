6a0cdc9c796cc36126b8d1b37e20c51f7af3c04a
Yurii Zinko <yurazinko@outlook.com>
2016-08-27 17:41:09 +0300

    next day click

--- script/script.js
+++ script/script.js
@@ -4,23 +4,35 @@ $(function() {
     var todayWeekDaySlot = document.getElementById('today-weekday');
     var todayMonthSlot = document.getElementById('today-month');
     var todayYearSlot = document.getElementById('today-year');
-    var calendar = { 
+    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    var calendar = {
         yesterdayDate: globalDate.getDate() - 1,
-        tomorrowDate: globalDate.getDate() + 1,
+        tomorrowDate: (function() {
+           return globalDate.getDate() + 1;
+        })(),
         todayDate: globalDate.getDate(),
         weekDay: (function() {
-        	var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-        	return days[globalDate.getDay()];
+            return days[globalDate.getDay()];
         })(),
         month: (function() {
-        	var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-        	return months[globalDate.getMonth()];
+            return months[globalDate.getMonth()];
         })(),
         year: globalDate.getFullYear()
     }
-    
+
     todayDateSlot.innerHTML = calendar.todayDate;
     todayWeekDaySlot.innerHTML = calendar.weekDay;
     todayMonthSlot.innerHTML = calendar.month;
     todayYearSlot.innerHTML = calendar.year;
+
+    $('#tomorrow-block').click(function() {
+        todayDateSlot.innerHTML = calendar.tomorrowDate;
+        tomorrowDateSlot.innerHTML = 
+    });
+
+    $('#yesterday-block').click(function() {
+        alert('working');
+    })
+
 });
