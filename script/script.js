dc9ac7f4fe1eb7a7ccbd0c01580d1c7e7108169a
Yurii Zinko <yurazinko@outlook.com>
2016-09-20 13:40:39 +0300

    connect angularjs

--- script/script.js
+++ script/script.js
@@ -1,34 +1,60 @@
 $(function() {
     var globalDate = new Date();
+    var calendar = angular.module('calendarApp', []);
+    calendar.controller('calendarCtrl', function($scope) {
+        $scope.todayDate = globalDate.getDate();
+        $scope.test = 'hello world';
+    })
+})
+
+
+
+
+
+
+
+
+
+/*
+$(function() {
+    var globalDate = new Date();
+
     var todayDateSlot = document.getElementById('today-date');
     var todayWeekDaySlot = document.getElementById('today-weekday');
     var todayMonthSlot = document.getElementById('today-month');
     var todayYearSlot = document.getElementById('today-year');
+    
+    var tomorrowDateSlot = document.getElementById('tomorrow-date');
+
     var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    var calendar = {
-        yesterdayDate: globalDate.getDate() - 1,
-        tomorrowDate: (function() {
-           return globalDate.getDate() + 1;
-        })(),
-        todayDate: globalDate.getDate(),
-        weekDay: (function() {
-            return days[globalDate.getDay()];
-        })(),
-        month: (function() {
-            return months[globalDate.getMonth()];
-        })(),
-        year: globalDate.getFullYear()
-    }
 
-    todayDateSlot.innerHTML = calendar.todayDate;
-    todayWeekDaySlot.innerHTML = calendar.weekDay;
-    todayMonthSlot.innerHTML = calendar.month;
-    todayYearSlot.innerHTML = calendar.year;
+    var yesterdayDate = globalDate.getDate() - 1;
+    var tomorrowDate = (function() {
+        return globalDate.getDate() + 1;
+    })();
+
+    var todayDate = globalDate.getDate();
+    var weekDay = (function() {
+        return days[globalDate.getDay()];
+    })();
+
+    var month = (function() {
+        return months[globalDate.getMonth()];
+    })();
+    var year = globalDate.getFullYear();
+
+
+    todayDateSlot.innerHTML = todayDate;
+    todayWeekDaySlot.innerHTML = weekDay;
+    todayMonthSlot.innerHTML = month;
+    todayYearSlot.innerHTML = year;
+
+    tomorrowDateSlot.innerHTML = tomorrowDate;
 
     $('#tomorrow-block').click(function() {
-        todayDateSlot.innerHTML = calendar.tomorrowDate;
-        tomorrowDateSlot.innerHTML = 
+        todayDateSlot.innerHTML = tomorrowDate;
+
     });
 
     $('#yesterday-block').click(function() {
@@ -36,3 +62,4 @@ $(function() {
     })
 
 });
+*/
